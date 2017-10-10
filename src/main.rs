ea0b5fe2927d71029819f9d9171277643b337c7c
Brad Gibson <brad@humanenginuity.com>
2017-10-09 23:22:06 -0700

    WIP: interim commit

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,33 @@
 #[macro_export]
-macro_rules! main {
-    ($main:expr) => {
-        fn main() {
-            use ::std::io::Write;
-
-            ::std::process::exit(match $main() {
-                Ok(r) => $crate::ExitCode::code(r),
-                Err(ref e) => {
-                    write!(&mut ::std::io::stderr(), "{}", )
-                        .expect("Error writing to stderr");
-
-                    1
-                }
-            });
-        }
-    };
-}
-
-/// Represents a value that can be used as the exit status of the process.
-/// See [`quick_main!`](macro.quick_main.html).
-pub trait ExitCode {
-    /// Returns the value to use as the exit status.
-    fn code(self) -> i32;
-}
-
-impl ExitCode for i32 {
-    fn code(self) -> i32 {
-        self
-    }
-}
-
-impl ExitCode for () {
-    fn code(self) -> i32 { EXIT_SUCCESS }
-}
 
 macro_rules! def_err {
-    //Unit error type
-    // allow trailing comma on last error definition, given explicit typename
+    // Unit error type
+    // allow trailing comma on last error definition, given explicit error typename
     ( $err_type:ty { $( $err:ident ),+ ,} ) => {
         def_err! { $err_type { $( $err ),+ } };
     };
 
-    // Allow trailing comma on last error definition, given no typename
+    // Allow trailing comma on last error definition, given no explicit error typename
     ( $( $err:ident ),+ ,) => {
         def_err! { $( $err ),+ };
     };
 
-    // Provide `Error` as the default typename for the "Error" type when none is specified
+    // Provide `Error` as the default error typename when an explicit error typename is not specified
     ( $( $err:ident ),+ ) => {
         def_err! { Error { $( $err ),+ } };
     };
 
-    // Define error variants given explicit typename
+    // Define error variants given an explicit error typename
     ( $err_type:ty { $( $err:ident ),+ } ) => {
         let crate_name = String::from(env!("CARGO_PKG_NAME"));
         let error_type = stringify!($err_type);
         let errs = vec![ $( stringify!($err) ),+ ];
-        println!("{}::{}::{:?}", $crate, error_type, errs);
+        println!("{}::{}::{:?}", crate_name, error_type, errs);
     };
 }
 
-pub fn main() {
+fn main() {
     def_err! {
-        UnitVariant,
-//        UnitVariant2(),
-    };
+        SampleErr1,
+    }
 }
