b045553d4537ea702a39c6279699e8f35cbbebb7
Brad Gibson <brad@humanenginuity.com>
2017-10-18 10:20:21 -0700

    WIP: attempting to inject `enum Error { vardecl, ... }` source

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,10 @@ macro_rules! def_err {
             parse_vardecls!($( $err_vardecls )+);
         //}
 
+        // Build description
+//        println!("build description: {}", stringify!($err_typename));
+//        format!("{}::{}::", env!("CARGO_PKG_NAME"), stringify!($err_typename))
+
         // Build display
 //        println!("build display: {}", stringify!($err_typename));
 
@@ -26,9 +30,7 @@ macro_rules! def_err {
     };
 
     // Re-invoke def_err! with `Error` as the default typename for the "Error" type when no typename is specified
-    ($err_variant:ident $($rest:tt)*) => {
-        def_err!(Error { $err_variant $($rest)* });
-    };
+    ($err_variant:ident $($rest:tt)*) => { def_err!(Error { $err_variant $($rest)* }); };
 }
 
 // Parse variant declarations
@@ -37,11 +39,11 @@ macro_rules! parse_vardecls {
     () => {};
 
     // Consume vardecl's trailing comma
-    (, $($rest:tt)*) => { parse_vardecls!($($rest)*); };
+    (, $($rest:tt)*) => { , parse_vardecls!($($rest)*); };
 
     // Process one vardecl, then re-invoke parse_variants! to parse remaining vardecls
     ($ident:ident $args:tt $($rest:tt)*) => {
-//        println!("parse_vardecls:\tident: {}\n\targs: {}", stringify!($ident), stringify!($args));
+        $ident
         parse_variant!($ident $args);
         parse_vardecls!($($rest)*);
     };
@@ -52,20 +54,13 @@ macro_rules! parse_variant {
     () => {};
 
     // Vardecl has no args
-    ($ident:ident ,) => {
-        println!("parse_variant: ident: {}", stringify!($ident));
-    };
+    ($ident:ident ,) => {};
 
     // Vardecl has no args (empty parentheses)
-    ($ident:ident ()) => {
-        println!("parse_variant: ident(): {}", stringify!($ident));
-    };
+    ($ident:ident ()) => { () };
 
     // Vardecl has args
-    ($ident:ident ($($args:tt)*)) => {
-        println!("parse_variant:\tident(args): {}", stringify!($ident));
-        parse_args!($($args)*)
-    };
+    ($ident:ident ($($args:tt)*)) => { ( parse_args!($($args)*) ) };
 }
 
 macro_rules! parse_args {
@@ -73,23 +68,23 @@ macro_rules! parse_args {
     () => {};
 
     // Consume arg's trailing comma
-    (, $($rest:tt)*) => { parse_args!($($rest)*); };
+    (, $($rest:tt)*) => { , parse_args!($($rest)*); };
 
     // Parse extended parameters (triggered by ;)
-    (; $($rest:tt)*) => { parse_extparams!($($rest)*); };
+    (; $($rest:tt)*) => { /*parse_extparams!($($rest)*);*/ };
 
     // Parse extended parameters (triggered by { causal_error })
-    ({ $cause:ty }) => { parse_extparams!($cause,); };
+    ({ $cause:ty }) => { /*parse_extparams!($cause,);*/ };
 
     // Parse extended parameters (triggered by Display str)
-    ($($rest:expr)*) => { parse_extparams!($($rest)*); };
+    ($($rest:expr)*) => { /*parse_extparams!($($rest)*);*/ };
 
     // Parse extended parameters (triggered by { causal_error }, Display str)
-    ({ $cause:ty }, $($rest:tt)*) => { parse_extparams!($cause, $($rest)*); };
+    ({ $cause:ty }, $($rest:tt)*) => { /*parse_extparams!($cause, $($rest)*);*/ };
 
-    // Process one ident: type arg, then re-invoke parse_args! to parse remaining args
+    // Process one `ident: type` arg, then re-invoke parse_args! to parse remaining args
     ($ident:ident : $type:tt $($rest:tt)*) => {
-        println!("\tparse_args: ident: type: {}: {}", stringify!($ident), stringify!($type));
+        $ident: $type
         parse_args!($($rest)*);
     };
 }
